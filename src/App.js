0c5301610cbd1f70571ae18aed365de5d725a17f
wolz-CODElife <joeladewole3@gmail.com>
2021-10-03 04:51:26 +0100

    Pushing to git

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import db from './firebase';
 import Video from './Video';
 
 function App() {
-  const [videos, setVideos] = useState([])
-  useEffect(() => {
-    db.collection('videos').onSnapshot((snapshot) =>
-      setVideos(snapshot.docs.map(doc => doc.data()))
-    )
-  }, [videos])
+  const [videos ] = useState([
+    {
+      url: 'https://res.cloudinary.com/codelife/video/upload/v1633232723/tiktok-clone/tiktok2_qxafx3.mp4',
+      channel: 'DanceCrew',
+      description: 'Video by Lara Jameson from Pexels',
+      song: 'Bounce - Ruger',
+      likes: 250,
+      messages: 120,
+      shares: 40
+    },
+    {
+      url: 'https://res.cloudinary.com/codelife/video/upload/v1633232725/tiktok-clone/tiktok1_np37xq.mp4',
+      channel: 'Happyfeet',
+      description: '#happyfeetlegwork videos on TikTok',
+      song: 'Kolo sound - Nathan',
+      likes: 250,
+      messages: 120,
+      shares: 40
+    },
+    {
+      url: 'https://res.cloudinary.com/codelife/video/upload/v1633232726/tiktok-clone/tiktok3_scmwvk.mp4',
+      channel: 'thiskpee',
+      description: 'The real big thug boys💛🦋 The real big thug boys💛🦋 ',
+      song: 'original sound - KALEI KING 🦋',
+      likes: 250,
+      messages: 120,
+      shares: 40
+    },
+  ])
   return (
     <div className="app">
       <div className="app_videos">
